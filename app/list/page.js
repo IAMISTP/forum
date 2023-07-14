87dcef68a756dde1b83a46fdde26e04c25a864fa
박혜민 <hyeminpak@gmail.com>
2023-07-14 21:58:10 +0900

    mongoDB 에 있는 데이터 가져와서 view 표출하기

---
+++ app/list/page.js
@@ -0,0 +1,20 @@
+import { connectDB } from "@/util/database";
+
+export default async function List() {
+  const client = await connectDB;
+  const db = client.db("forum");
+  let result = await db.collection("post").find().toArray();
+  console.log(result);
+  return (
+    <div className="list-bg">
+      {result.map((x, i) => {
+        return (
+          <div className="list-item">
+            <h4>{result[i].title}</h4>
+            <p>{result[i].content}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
